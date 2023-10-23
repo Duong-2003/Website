050e3dfbfc192426c32a5ca092ad54561fc3e1fd
Ndd <duonghumg145@gmail.com>
2023-10-23 15:25:04 +0700

    code succinct cotent

--- sources/FE/content.php
+++ sources/FE/content.php
@@ -14,11 +14,6 @@
     text-decoration: none;
   }
 
-  .card img {
-    /* height: 200px; */
-
-
-  }
 
   .card {
     box-shadow: 0 0 5px 0px;
@@ -59,32 +54,6 @@
 
 <body>
 
-
-
-
-  <!-- <div class="Slider1 py-4">
-            <div class="container">
-            <hr style="color:red">
-                <div class="row content" style="text-align:center ;">
-                  
-
-                  <div class=" col-lg-1 col-md-2 col-sm-2">
-                        <a href="" style="color:coral">
-                        <img src=<?= $linkImgSp . "ip1menu.webp" ?>alt="">
-                        <p>Samsung Galaxy Z Flip5</p>
-                        </a>
-                    </div>
-                   
-                   
-                 
-                      </div>
-                      
-
-            </div> -->
-
-
-
-  <!-- ----------------------- -->
   <div class="product-list mb-3 p-2 ">
     <div class="container">
       <div class="row">
@@ -100,179 +69,51 @@
       </div>
     </div>
 
-    <!-- _____________________________________ -->
     <?php
 
     include_once($linkconnWebsite);
-
-    $sql = "SELECT * FROM sanpham  LIMIT 8";
+    $valueCart = 8;
+    $sql = "SELECT * FROM sanpham LIMIT $valueCart";
     $result = $connect->query($sql);
     $duongdanimg = $linkImgSp;
 
-    $data = array();
+    $dataArray = array();
     if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
+        $dataArray[] = $row;
       }
     }
     // Đóng kết nối
     $connect->close();
 
     ?>
-    <script>
-      var data = <?php echo json_encode($data); ?>;
+   
+    <!-- ________________Show thẻ_____________________ -->
+    <div class="container text-center">
+      <div class="row">
 
-      function myFunction() {
-        if (data.length < 6) {
-          var valueCardReplace = 6 - data.length;
-          var newElement = document.getElementById("cardReplace");
-          for (var i = 1; i <= valueCardReplace; i++) {
-            var oldElement = document.getElementById("card" + i);
-            if (newElement && oldElement) {
-              var clonedElement = newElement.cloneNode(true);
-              clonedElement.style.display = 'block';
-              oldElement.parentNode.replaceChild(clonedElement, oldElement);
-            }
-          }
-        }
-      }
-      document.addEventListener("DOMContentLoaded", myFunction);
-    </script>
-
-    <!-- <div id="cardReplace" class="card" aria-hidden="true" style="display: none;">
-      <img src="#" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title placeholder-glow">
-          <span class="placeholder col-6"></span>
-        </h5>
-        <p class="card-text placeholder-glow">
-          <span class="placeholder col-7"></span>
-          <span class="placeholder col-4"></span>
-          <span class="placeholder col-4"></span>
-          <span class="placeholder col-6"></span>
-          <span class="placeholder col-8"></span>
-        </p>
-        <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
-      </div>
-    </div> -->
-
-    <!-- ________________Show ther_____________________ -->
-    <div class=" container text-center">
-      <div class="row ">
-
-
-
-
-
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[0]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[0]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[0]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[0]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
+        <?php foreach ($dataArray as $data) : ?>
+         
+          <div class="col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
+            <div id="card<?= $i ?>" class="card">
+              <img src="<?= $duongdanimg . $data['sp_img'] ?>" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Tên sản phẩm:<?= $data['sp_ten'] ?></p>
+                <p class="card-text">Giá sản phẩm:<?= $data['sp_gia'] ?></p>
+                <a href="./product.php?sp_ma=<?= $data['sp_ma'] ?>" class="btn btn-primary">Xem</a>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[1]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[1]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[1]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[1]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[1]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[1]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[1]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[1]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[1]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[1]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[1]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[1]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[1]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[1]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[1]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[1]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[1]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[1]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[1]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[1]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[1]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[1]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[1]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[1]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-
-        <div class=" col-lg-3 col-md-4 col-sm-6 py-2" id="font-card">
-          <div id='card3' class="card">
-            <img src="<?= $duongdanimg . $data[2]['sp_img'] ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-title">Tên sản phẩm:<?= $data[2]['sp_ten'] ?></p>
-              <p class="card-text">Giá sản phẩm:<?= $data[2]['sp_gia'] ?></p>
-              <a href="./product.php?sp_ma=<?= $data[2]['sp_ma'] ?>" class="btn btn-primary">Xem</a>
-            </div>
-          </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <?php endforeach; ?>
 
       </div>
     </div>
-  </div>
+
+
+    <div class="text-center p-3">
+      <a class="btn btn-primary" href="./resetpass.php">Xem thêm</a>
+    </div>
   </div>
 
 
 </body>
-
-</html>
