b96995d53a2c3fc39dfafbdf80cc85ad6ee09e6b
Dươngg <anhtustyle2003@gmail.com>
2023-11-27 21:39:35 +0700

    fix font footer_save

--- sources/FE/footer_save.php
+++ sources/FE/footer_save.php
@@ -1,11 +1,12 @@
 
- <style>
+    <style>
          
 .swiper-slide {
   display: inline-block; /* Hiển thị các phần tử trên cùng một hàng */
   vertical-align: top; /* Căn chỉnh các phần tử theo phía trên */
   margin-right: 20px; /* Khoảng cách giữa các phần tử */
 }
+
     </style>
 </head>
 <body>
@@ -16,12 +17,12 @@
 				<div class="swiper-slide swiper-slide-active" style="width: 286.5px; margin-right: 30px;">
 					<div class="item">
 						<div class="image py-2 " style="text-align: center;">
-							<img width="150" height="150" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service1.png?1700191277185" alt="Cảm kết chất lượng" data-was-processed="true">
+							<img width="130" height="130" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service1.png?1700191277185" alt="Cảm kết chất lượng" data-was-processed="true">
 						
-							<h3>
+							<h3 style="color:#91a161">
 								Cam kết chất lượng
-							</h3>
-							<p>
+							</h3 >
+							<p style="color:#91a161">
 								Bồi hoàn gấp 10 lần giá trị sản phẩm nếu phát hiện hàng giả.
 							</p>
 						</div>
@@ -30,13 +31,13 @@
 				<div class="swiper-slide swiper-slide-next" style="width: 286.5px; margin-right: 30px;">
 					<div class="item">
 						<div class="image py-2 " style="text-align: center;">
-							<img width="150" height="150" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service2.png?1700191277185" alt="Đảm bảo vệ sinh ATTP" data-was-processed="true">
+							<img width="130" height="130" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service2.png?1700191277185" alt="Đảm bảo vệ sinh ATTP" data-was-processed="true">
 						
-							<h3>
-								Đảm bảo  chính hãng
-							</h3>
-							<p>
-								Bao đổi trả
+							<h3 style="color:#91a161">
+								Đảm bảo chính hãng
+							</h3 >
+							<p style="color:#91a161">
+							Đổi trả sản phẩm trong vòng 24h kể từ thời điểm nhận hàng
 							</p>
 						</div>
 					</div>
@@ -44,12 +45,12 @@
 				<div class="swiper-slide" style="width: 286.5px; margin-right: 30px;">
 					<div class="item">
 						<div class="image py-2 " style="text-align: center;">
-							<img width="150" height="150" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service3.png?1700191277185" alt="Đổi trả sản phẩm" data-was-processed="true">
+							<img width="130" height="130" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service3.png?1700191277185" alt="Đổi trả sản phẩm" data-was-processed="true">
 						
-							<h3>
+							<h3 style="color:#91a161">
 								Đổi trả sản phẩm
-							</h3>
-							<p>
+							</h3 >
+							<p style="color:#91a161">
 								Đổi trả sản phẩm trong vòng 24h kể từ thời điểm nhận hàng
 							</p>
 						</div>
@@ -58,12 +59,12 @@
 				<div class="swiper-slide" style="width: 286.5px; margin-right: 30px;">
 					<div class="item">
 						<div class="image py-2 " style="text-align: center;">
-							<img width="150" height="150" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service4.png?1700191277185" alt="Giao hàng" data-was-processed="true">
+							<img width="130" height="130" class="lazyload img-responsive loading" src="//bizweb.dktcdn.net/100/488/726/themes/911609/assets/image_service4.png?1700191277185" alt="Giao hàng" data-was-processed="true">
 						
-							<h3>
+							<h3 style="color:#91a161">
 								Giao hàng
-							</h3>
-							<p>
+							</h3 >
+							<p style="color:#91a161">
 								Giao hàng toàn quốc, thanh toán COD ngay tại nơi nhận hàng.
 							</p>
 						</div>
